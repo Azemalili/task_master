cd2afa8d6a5b621bc0473cbcbbbf015040a55e31
Azem Alili <aliliazem01@gmail.com>
2024-07-21 23:50:29 +0200

    .

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,22 +1,17 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABSE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyBkTsn0dkLoatFOIT5diyYhR6SJ8Q7FEXg",
+  authDomain: "task-master-a5bf6.firebaseapp.com",
+  databaseURL: "https://task-master-a5bf6-default-rtdb.europe-west1.firebasedatabase.app/",
+  projectId: "task-master-a5bf6",
+  storageBucket: "task-master-a5bf6.appspot.com",
+  messagingSenderId: "838147793471",
+  appId: "1:838147793471:web:96d529c017351d6a68ce8a"
 };
 
 // Initialize Firebase
-export default app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const app = initializeApp(firebaseConfig);
+
+export default app
